bac480a40fd1aa31660c6acab246c72acc953c72
calmery <contact@calmery.me>
2017-03-19 20:28:01 +0900

    Fix regex for variable name

--- token.js
+++ token.js
@@ -3,7 +3,7 @@ const token = {
     space   : /\s+/,
     bracket : /[\(|\)]/,
     bondage : /\=/,
-    variable: /[a-z|A-Z|_|$][a-z|A-Z|0-9|_|$]+/,
+    variable: /[a-z|A-Z|_|$][a-z|A-Z|0-9|_|$]*/,
     braces  : /\{|\}/,
     newline : /\n+/
 }
